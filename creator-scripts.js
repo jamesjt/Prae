497641a6ca2e0bb85a97dde4e487a4646b8329f4
jamesjt <jtjamesni@gmail.com>
2025-11-22 23:20:38 -0800

    attribute points

--- creator-scripts.js
+++ creator-scripts.js
@@ -371,10 +371,9 @@ function updateAttributeGroups() {
 function updateAttributeGroup(group) {
     const level = parseInt(document.getElementById('charLvl').value) || 1;
     const pri = document.getElementById(group.priorityId).value || '3';
-    let totalPoints = level;
-    if (pri === '1') totalPoints += 2;
-    else if (pri === '2') totalPoints += 1;
-    // ter = level
+    let totalPoints = 3 + Math.floor((level + 1) / 3);
+    if (pri === '2') totalPoints = 2 + Math.floor(level / 3);
+    else if (pri === '3') totalPoints = 1 + Math.floor((level - 1) / 3);
 
     const primaryMax = parseInt(document.getElementById(group.primaryValueId).innerText) || 0;
 
